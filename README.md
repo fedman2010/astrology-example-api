c8c41b6baaee1998ca148cc24110e3fdabfa05aa
MaksymD <maksym@robarov.be>
2020-05-28 19:54:26 +0300

    Write readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Astrology project
+# Astrology project
+
+
+###### Prerequisite
+docker and docker-compose
+
+
+###### Instalation
+ 1. Run             `cp .env.example .env` 
+ 2. Run             `docker-compose up -d`
+ 3. Run composer    `docker-compose exec app composer install`
+ 4. Run migrations  `docker-compose exec app php artisan migrate`
+ 5. Run seeders     `docker-compose exec app php artisan db:seed`
+ 
+ If on step 4 you don't have database, do this:
+ 1. `docker-compose exec db /bin/sh`
+ 2. `mysql -u root -psecret`
+ 2. `create schema homestead;`
+ 3. `exit` 
+ 
+###### Postman Public Link
+https://www.getpostman.com/collections/0b3f4bf3a8b979e24f0c
--- routes/web.php
+++ routes/web.php
@@ -13,29 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/read', function (\App\Services\Google\Sheets $sheets) {
-//dd(getenv('POST_SPREADSHEET_ID'), "'" . getenv('POST_SPREADSHEET_ID') . "'");
-//    $s = Sheets::spreadsheet('1dbJ3RSByffQebmWDa1EcrlUdDAlJoUdySy_f4UdP7Xs');
-//    $data = $s->sheet("'tab1'")->get();
+//Route::get('/read', function (\App\Services\Google\Sheets $sheets) {
+//    /*
+//     * test read from Gooogle Sheets API
+//     */
 //
-    $data = $sheets->read();
-
-    ddd($data);
-});
-
-Route::get('/write', function (\App\Services\Google\Sheets $sheets) {
-    $fake = \Faker\Factory::create();
-
-    $order = new \App\Order(
-        [
-            'astrologer_id' => 1,
-            'service_id' => 1,
-            'email' => $fake->email,
-            'name' => $fake->name,
-        ]
-    );
-    $order->price = $fake->randomNumber(2);
-
-
-    $sheets->write($order);
-});
+//    $data = $sheets->read();
+//
+//    dd($data);
+//});
+//
+//Route::get('/write', function (\App\Services\Google\Sheets $sheets) {
+//    /*
+//     * Test write too Gooogle Sheets API
+//     */
+//    $fake = \Faker\Factory::create();
+//
+//    $order = new \App\Order(
+//        [
+//            'astrologer_id' => 1,
+//            'service_id' => 1,
+//            'email' => $fake->email,
+//            'name' => $fake->name,
+//        ]
+//    );
+//    $order->price = $fake->randomNumber(2);
+//
+//
+//    $sheets->write($order);
+//});
